719ec9b8e7ab1027a28b3e57d7191f749c546549
Ilia Kholopkin <iliagold@icloud.com>
2025-09-14 14:31:04 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import GraphBlock from "./components/GraphBlock.jsx";
+
+function App() {
+    return (<div><GraphBlock></GraphBlock></div>);
+}
+
+export default App
---
+++ src/components/GraphBlock.jsx
@@ -0,0 +1,7 @@
+import '../App.css'
+
+function GraphBlock() {
+    return (<div>GRAPH BLOCK</div>);
+}
+
+export default GraphBlock;
