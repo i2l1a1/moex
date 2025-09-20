b65647aa3dbc78cde0035e18da1fda9d8b6124d6
Ilia Kholopkin <iliagold@icloud.com>
2025-09-20 22:09:18 +0300

    AbortController in useFetch.jsx

--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function useFetch(url, requestParameters) {
-    console.log(requestParameters);
-
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        const abortController = new AbortController();
+        const signal = abortController.signal;
+
         async function fetchData(url) {
             try {
                 const res = await fetch(url, {
+                    signal: signal,
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify(requestParameters),
+                    body: JSON.stringify(requestParameters)
                 });
                 if (!res.ok) {
                     throw new Error(`HTTP ERROR: ${res.status}`);
@@ -23,16 +25,25 @@ function useFetch(url, requestParameters) {
                 const data = await res.json();
                 setData(data);
             } catch (err) {
-                setError(err.message);
+                if (err.name === "AbortError") {
+                    //
+                } else {
+                    setError(err.message);
+                }
             } finally {
                 setLoading(false);
             }
         }
 
-        fetchData(url).then((r) => {});
+        fetchData(url).then((r) => {
+        });
+
+        return () => {
+            abortController.abort();
+        };
     }, [url, requestParameters]);
 
-    return { data, loading, error };
+    return {data, loading, error};
 }
 
 export default useFetch;
