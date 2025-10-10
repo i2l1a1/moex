4ead4d565463bbb8ee1c2133aa86abf9ee8606a0
Ilia Kholopkin <iliagold@icloud.com>
2025-10-10 23:05:03 +0300

    real 10% for prices

--- frontend/src/components/graph/calculatePriceTicks.js
+++ frontend/src/components/graph/calculatePriceTicks.js
@@ -2,15 +2,14 @@ export function calculatePriceTicks(processedData, tickCount = 5) {
     const priceValues = processedData.map((item) => item.cost);
     const minPrice = Math.min(...priceValues);
     const maxPrice = Math.max(...priceValues);
-    const priceRange = maxPrice - minPrice;
-    const priceDomainMin = Math.floor(minPrice - priceRange * 0.1);
-    const priceDomainMax = Math.ceil(maxPrice + priceRange * 0.1);
+    const priceDomainMin = Math.round(minPrice * 0.9);
+    const priceDomainMax = Math.round(maxPrice * 1.1);
 
     const tickInterval = (priceDomainMax - priceDomainMin) / (tickCount - 1);
 
     const priceTicks = [];
     for (let i = 0; i < tickCount; ++i) {
-        priceTicks.push(Math.round(priceDomainMin + i * tickInterval));
+        priceTicks.push(priceDomainMin + i * tickInterval);
     }
 
     return {
