8c8b730444abe0fea6e0dc5483f2713619d650a8
Ilia Kholopkin <iliagold@icloud.com>
2025-12-03 15:23:59 +0300

    custom futures: ED

--- server/get_data.py
+++ server/get_data.py
@@ -14,6 +14,49 @@ ALGOPACK_API_KEY = env("ALGOPACK_API_KEY")
 
 
 class FetchMoexData:
+    def __build_custom_candles(self, builder, *responses):
+        return {"candles": builder(*responses)}
+
+    def __fetch_costs_for_ticker(self, ticker, moex_api_base_url, cur_from, cur_till, headers):
+        url_costs = "iss/engines/stock/markets/shares/boards/tqbr/securities"
+        url_type = cost_mapping[ticker].url_type
+
+        if url_type == "futures":
+            url_costs = "iss/engines/futures/markets/forts/boards/rfud/securities"
+        elif url_type == "index":
+            url_costs = "iss/engines/stock/markets/index/securities"
+        elif url_type == "currency":
+            url_costs = "iss/engines/currency/markets/selt/boardgroups/13/securities"
+
+        asset_code = cost_mapping[ticker].asset_code[0]
+        url_costs = f"{moex_api_base_url}/{url_costs}/{asset_code}/candles.json?from={cur_from}&till={cur_till}&interval=24"
+        return requests.get(url_costs, headers=headers).json()
+
+    def __build_custom_costs_ED(self, resp_costs_eu, resp_costs_si):
+        cols_eu = resp_costs_eu["candles"]["columns"]
+        cols_si = resp_costs_si["candles"]["columns"]
+        data_eu = resp_costs_eu["candles"]["data"]
+        data_si = resp_costs_si["candles"]["data"]
+
+        idx_close_eu = cols_eu.index("close")
+        idx_begin_eu = cols_eu.index("begin")
+        idx_close_si = cols_si.index("close")
+
+        rows = []
+        for row_eu, row_si in zip(data_eu, data_si):
+            cost_eu = row_eu[idx_close_eu]
+            cost_si = row_si[idx_close_si]
+            traded_begin = row_eu[idx_begin_eu]
+
+            if cost_eu is None or cost_si in (0, None):
+                synthetic_cost = None
+            else:
+                synthetic_cost = round(cost_eu / cost_si, 3)
+
+            rows.append([synthetic_cost, traded_begin])
+
+        return {"columns": ["close", "begin"], "data": rows}
+
     def __fetch_json_from_moex(self, ticker, from_data, till_date):
         _min_date = date(2007, 1, 1)
         try:
@@ -46,20 +89,24 @@ class FetchMoexData:
 
             url_numbers = f"{moex_api_base_url}/iss/analyticalproducts/futoi/securities/{ticker}.json?from={cur_from}&till={cur_till}&latest=1"
 
-            url_costs = "iss/engines/stock/markets/shares/boards/tqbr/securities"
-            if cost_mapping[ticker].url == "futures":
-                url_costs = "iss/engines/futures/markets/forts/boards/rfud/securities"
-            elif cost_mapping[ticker].url == "custom":
-                pass
-            elif cost_mapping[ticker].url == "index":
-                url_costs = "iss/engines/stock/markets/index/securities"
-            elif cost_mapping[ticker].url == "currency":
-                url_costs = "iss/engines/currency/markets/selt/boardgroups/13/securities"
-
-            url_costs = f"{moex_api_base_url}/{url_costs}/{cost_mapping[ticker].ticker}/candles.json?from={cur_from}&till={cur_till}&interval=24"
-
             resp_numbers = requests.get(url_numbers, headers=headers).json()
-            resp_costs = requests.get(url_costs, headers=headers).json()
+
+            if cost_mapping[ticker].url_type != "custom":
+                resp_costs = self.__fetch_costs_for_ticker(ticker, moex_api_base_url, cur_from, cur_till, headers)
+            else:
+                if ticker == "ED":
+                    base_eu, base_si = cost_mapping[ticker].asset_code
+                    resp_costs_eu = self.__fetch_costs_for_ticker(
+                        base_eu, moex_api_base_url, cur_from, cur_till, headers
+                    )
+                    resp_costs_si = self.__fetch_costs_for_ticker(
+                        base_si, moex_api_base_url, cur_from, cur_till, headers
+                    )
+                    resp_costs = self.__build_custom_candles(
+                        self.__build_custom_costs_ED, resp_costs_eu, resp_costs_si
+                    )
+                else:
+                    resp_costs = {"candles": {"columns": ["close", "begin"], "data": []}}
 
             if numbers_cols is None:
                 numbers_cols = resp_numbers['futoi']['columns']
